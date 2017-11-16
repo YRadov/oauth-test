386fffcbfbfcd6ba5740132b31a379d4aa9f3c95
YRadov <radov.yuriy@ukr.net>
2017-11-16 02:30:50 +0200

    test

--- public/index.php
+++ public/index.php
@@ -6,12 +6,13 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config/config.php';
 require __DIR__ . '/../app/Helpers/functions.php';
 
+debug($_SERVER);
 $router = Router::getInstance();
 
 $controller = $router->getController();
 
 $action = $router->getAction();
-
+dd([$controller, $action]);
 $controller = new $controller;
 $controller->$action();
 
